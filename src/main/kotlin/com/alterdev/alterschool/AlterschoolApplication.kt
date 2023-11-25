d67b8ea2e91f38576e5ef4cca4eb45a29c6880bc
Muhamad Abdul Aziz <azizgimboell@gmail.com>
2023-11-25 18:56:42 +0700

    First init

---
+++ src/main/kotlin/com/alterdev/alterschool/AlterschoolApplication.kt
@@ -0,0 +1,11 @@
+package com.alterdev.alterschool
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlterschoolApplication
+
+fun main(args: Array<String>) {
+	runApplication<AlterschoolApplication>(*args)
+}
---
+++ src/test/kotlin/com/alterdev/alterschool/AlterschoolApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alterdev.alterschool
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlterschoolApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
